9b07bf3a5f00fabd019f53d839d4efc604888835
Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>
2025-02-13 10:11:14 +0530

    Separate llm functions from the handler
    Signed-off-by: Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>

---
+++ llm.py
@@ -0,0 +1,13 @@
+import os
+
+from model import Server
+
+
+def ask_llm(question: str, server: Server) -> str:
+    response = server.llm.chat.completions.create(
+        model=os.environ.get('MODEL_TO_USE'),
+        messages=[
+            {"role": "user", "content": question}
+        ],
+    )
+    return response.choices[0].message.content
