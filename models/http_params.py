6f2f85a0f23a38b7c766947d200771909da36638
Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>
2025-02-20 14:31:47 +0530

    Let the category and comment be optional
    Signed-off-by: Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>

--- models/http_params.py
+++ models/http_params.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -8,8 +10,8 @@ class QueryRequest(BaseModel):
 class FeedbackRequest(BaseModel):
     span_id: str
     feedback: int
-    category: str
-    comment: str
+    category: Optional[str] = ''
+    comment: Optional[str] = ''
 
 
 class ReportRequest(BaseModel):
