9503593968bc2d5b7d23a2c0a7c84d865d51fb21
Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>
2025-02-20 13:17:17 +0530

    Use POST /feedback instead of /feedback/create
    Signed-off-by: Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>

--- services/feedback/router.py
+++ services/feedback/router.py
@@ -11,7 +11,7 @@ from services.feedback.handler import mark_user_feedback, get_feedback_summary
 def get_router() -> APIRouter:
     feedback_router = APIRouter(prefix="/feedback", tags=["feedback"])
 
-    @feedback_router.post('/create')
+    @feedback_router.post('/')
     def user_feedback(request: FeedbackRequest):
         if request.feedback not in (0, 1):
             return JSONResponse("feedback must be either 0/1", http.HTTPStatus.BAD_REQUEST)
